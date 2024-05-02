a74db1f185edbcfb1967fba3344a4fbdd27ed869
Kapil Amin <kapilamin@Kapils-Laptop.local>
2024-05-02 11:15:18 +0100

    Task 4 - can view a list of available items

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 export default function Home(){
     return(
-    <h2>Welcome to NC Marketplace</h2>
+    <h2 className="Home">Welcome to NC Marketplace</h2>
 )
 }
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -7,26 +7,26 @@ export function ItemList (){
 
     useEffect(()=>{
         fetch(`https://nc-marketplace-sem-1.onrender.com/api/items`)
-        .then((response)=> response.json)
+        .then(response => {return response.json()})
         .then((data)=>{
-            console.log(data);
-            {data.items.map((item)=>{
-            setItems(
-                <li key={item.item_id}>
-                    <p>{item.item_name}</p>
-                </li>
-            )
-            })}
-            console.log(data.items);
-        })
-    }, [items])
+            if (data && data.items) {
+                const listItems = data.items.map(item => ( 
+                    <li key={item.item_id} className="itemList">
+                        <p>{item.item_name}</p>
+                        <img src={item.img_url} alt={item.item_name} style={{ width: '150px', height: '150px' }} />
+                        <p>Category: {item.category_name}</p>
+                        <p>Description: {item.description}</p>
+                        <p>Price: £{item.price / 100}</p>
+                    </li>
+                ));
+                setItems(listItems);}
+        })    
+    }, [])
     
    
     return(
-        
         <ul>
-            <p>"HELLO"</p>
-            
+        {items}
         </ul>
         
     )
